092eaa7712985abbc8464203f0bf435165707e09
mrwhyyouareyou-arch <mrwhyyouareyou@gmail.com>
2026-04-16 14:54:50 +0500

    Add caching for install and fetch events
    Implement caching strategy for service worker.

--- sw.js
+++ sw.js
@@ -1,8 +1,38 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => {
+const CACHE = "streamx-v2";
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("/index.html")))
   );
 });
-// 
